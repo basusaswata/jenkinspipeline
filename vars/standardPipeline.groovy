ef56c164577524255ed9a9c66170dbbbf8e1037f
basusaswata <basu.saswata@gmail.com>
2017-11-25 17:13:21 -0600

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -3,7 +3,7 @@
         def config = [:]
         body.resolveStrategy = Closure.DELEGATE_FIRST
         body.delegate = config
-        body()
+        
 
         node {
             // Clean workspace before doing anything
